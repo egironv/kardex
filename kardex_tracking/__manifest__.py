86e295d7f45bf76bd236a367758aef96beb0f42d
Elder Giron <egiron.valladares@gmail.com>
2024-06-30 14:22:14 -0600

    [ADD CHANGUES PAID]

--- kardex_tracking/__manifest__.py
+++ kardex_tracking/__manifest__.py
@@ -1,17 +1,21 @@
 {
-    'name': 'Product Kardex Wizard2',
-    'version': '1.0',
+    'name': 'Kardex de Productos',
+    'version': '15.0',
     'summary': 'Wizard para visualizar movimientos de Kardex',
     'description': 'Este módulo permite visualizar los movimientos de Kardex de un producto en un rango de fechas.',
     'category': 'Inventory',
-    'author': 'Tu Nombre',
+    'author': 'Elder Armando Giron',
+    'price': '48.99',
+    'currency': 'USD',
     'website': '',
     'depends': ['stock', 'point_of_sale'],
     'data': [
+        'security/ir.model.access.csv',
         'views/product_kardex_wizard_views.xml',
         'views/menu_views.xml',
         'views/kardex_tracking_views.xml'
     ],
+    'images':['static/description/banner.gif'],
     'installable': True,
     'application': False,
 }
